ce9ff854cdd973a36431048f4ceae7e543b7a117
Gabrielle Lattanzi <g.lattanzi26@gmail.com>
2024-05-07 20:19:12 -0300

    Project initial configuration

---
+++ README.md
@@ -0,0 +1,6 @@
+# GameAPI
+
+Commands used: 
+	Tools -> NuGet Package Manager -> Package Manager Console: 
+		dotnet add package Microsoft.Data.SqlClient
+		Install-Package Newtonsoft.Json
--- obj/Debug/net8.0/GameAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927717f345227d0b0d9f6ea014cacffdc1f0e567")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
